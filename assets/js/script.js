3a3a7530ad84337fd1eb9a2fad51639db27c4d82
Dipak Kumar <dkrray772@gmail.com>
2023-05-05 23:45:46 +0530

    crazy shit

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,15 @@ window.addEventListener("load", function(){
     circle.style.animation = "none";
     document.body.classList.add("loaded");
 })
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const togglerNavbar = function(){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navToggler, "click", togglerNavbar);
